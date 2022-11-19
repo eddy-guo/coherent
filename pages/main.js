40e9fedac0ca7ec4d5c7ed287ee849d427654904
Amey Dhamgunde <ameydhamgunde@yahoo.com>
2022-11-19 07:48:58 -0500

    Dynamic chatboxes
    mapping accross chatboxes

--- pages/main.js
+++ pages/main.js
@@ -1,6 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Main.module.css";
+import ChatBoxes from "./chatboxes";
+
+const testing_chat_msgs = [{
+  style: styles.leftmessage,
+  content: "This is a test message."
+},{
+  style: styles.rightmessage,
+  content: "This is a second test message."
+
+},{
+  style: styles.leftmessage,
+  content: "This is a third test message."
+
+},{
+  style: styles.rightmessage,
+  content: "This is a fourth test message."
+
+}];
 
 export default function Main() {
   return (
@@ -9,17 +27,7 @@ export default function Main() {
       <title>Co:herent - Main</title>
       </Head>
       <h1 className={styles.header}>Co:herent</h1>
-      <div className={styles.messages}>
-      {/* this should populate on its own when messages are sent (left, right, then left etc) */}
-      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
-      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
-      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
-      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
-      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
-      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
-      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
-      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
-      </div>
+      <ChatBoxes chat_messages={testing_chat_msgs} />
       <div className={styles.inputarea}>
         <input
           className={styles.input}
