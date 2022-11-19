37065bba86efca017c3d544c9439ad7d589ea10c
Eddy Guo <eddyyanruguo@gmail.com>
2022-11-19 05:10:32 -0500

    main application general format and setup

--- pages/main.js
+++ pages/main.js
@@ -1,13 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
-// import styles from "../styles/Home.module.css";
+import styles from "../styles/Main.module.css";
 
 export default function Main() {
   return (
-    <div>
-      <main>
-        <p>main page</p>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <Head>
+      <title>Co:herent - Main</title>
+      </Head>
+      <h1 className={styles.header}>Co:herent</h1>
+      <div className={styles.messages}>
+      {/* this should populate on its own when messages are sent (left, right, then left etc) */}
+      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
+      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
+      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
+      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
+      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
+      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
+      <div className={styles.leftmessage}><p className={styles.lefttext}>temporary message</p></div>
+      <div className={styles.rightmessage}><p className={styles.righttext}>temporary message</p></div>
+      </div>
+      <div className={styles.inputarea}>
+        <input
+          className={styles.input}
+          type="text"
+          id="input"
+          name="input-text"
+          placeholder="Input your text here"
+        />
+        <input className={styles.button} type="image" src="/images/microphone.svg" />
+      </div>
+    </main>
   );
 }
